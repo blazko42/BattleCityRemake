409b2fc9685e0f229d476c889f5501b5121e80fe
blazko42 <david.dumitru62@gmail.com>
2024-09-10 04:14:34 +0200

    added base

--- scripts/projectiles/ShellProjectile.cs
+++ scripts/projectiles/ShellProjectile.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Runtime.Serialization.Formatters;
 using Godot;
 
 public partial class ShellProjectile : Area2D
@@ -50,12 +51,18 @@ public partial class ShellProjectile : Area2D
     #region Signals
     public void OnShellProjectileBodyEntered(Node2D body)
     {
+        Explode();
+
         if (body is TileMapLayer && body.Name == "LevelAssets")
         {
             DestroyEnvironment((TileMapLayer)body);
         }
 
-        Explode();
+        if (body is CollisionShape2D && body.Name == "PrototypeBase")
+        {
+            body.QueueFree();
+            Destroy();
+        }
 
         CallDeferred(Node2D.MethodName.QueueFree);
     }
@@ -92,102 +99,107 @@ public partial class ShellProjectile : Area2D
 
         if (tileMapLayer.GetCellSourceId(tilePosition) != -1)
         {
-            if (ShellProjectileDirection == Vector2.Up || ShellProjectileDirection == Vector2.Down)
+            HandleEnvironmentDestruction(tileMapLayer, tilePosition);
+        }
+    }
+
+    //REFACTOR  
+    private void HandleEnvironmentDestruction(TileMapLayer tileMapLayer, Vector2I tilePosition)
+    {
+        if (ShellProjectileDirection == Vector2.Up || ShellProjectileDirection == Vector2.Down)
+        {
+            Vector2I leftFirstNeighborCell = tileMapLayer.GetNeighborCell(tilePosition, TileSet.CellNeighbor.LeftSide);
+            int leftFirstNeighborCellSourceId = tileMapLayer.GetCellSourceId(leftFirstNeighborCell);
+
+            Vector2I leftSecondNeighborCell = tileMapLayer.GetNeighborCell(leftFirstNeighborCell, TileSet.CellNeighbor.LeftSide);
+            int leftSecondNeighborCellSourceId = tileMapLayer.GetCellSourceId(leftSecondNeighborCell);
+
+            Vector2I rightFirstNeighborCell = tileMapLayer.GetNeighborCell(tilePosition, TileSet.CellNeighbor.RightSide);
+            int rightFirstNeighborCellSourceId = tileMapLayer.GetCellSourceId(rightFirstNeighborCell);
+
+            var side = leftFirstNeighborCellSourceId == -1 || leftSecondNeighborCellSourceId == -1 ? "left" :
+            rightFirstNeighborCellSourceId == -1
+            ? "right" :
+            "middle";
+
+            if (side == "middle")
             {
-                Vector2I leftFirstNeighborCell = tileMapLayer.GetNeighborCell(tilePosition, TileSet.CellNeighbor.LeftSide);
-                int leftFirstNeighborCellSourceId = tileMapLayer.GetCellSourceId(leftFirstNeighborCell);
-
-                Vector2I leftSecondNeighborCell = tileMapLayer.GetNeighborCell(leftFirstNeighborCell, TileSet.CellNeighbor.LeftSide);
-                int leftSecondNeighborCellSourceId = tileMapLayer.GetCellSourceId(leftSecondNeighborCell);
-
-                Vector2I rightFirstNeighborCell = tileMapLayer.GetNeighborCell(tilePosition, TileSet.CellNeighbor.RightSide);
-                int rightFirstNeighborCellSourceId = tileMapLayer.GetCellSourceId(rightFirstNeighborCell);
-
-                var side = leftFirstNeighborCellSourceId == -1 || leftSecondNeighborCellSourceId == -1 ? "left" :
-                rightFirstNeighborCellSourceId == -1
-                ? "right" :
-                "middle";
-
-                if (side == "middle")
+                tileMapLayer.SetCell(leftSecondNeighborCell, -1);
+                tileMapLayer.SetCell(leftFirstNeighborCell, -1);
+                tileMapLayer.SetCell(tilePosition, -1);
+                tileMapLayer.SetCell(rightFirstNeighborCell, -1);
+            }
+            if (side == "left")
+            {
+                if (leftFirstNeighborCellSourceId == -1)
+                {
+                    tileMapLayer.SetCell(tilePosition, -1);
+                    tileMapLayer.SetCell(rightFirstNeighborCell, -1);
+                }
+                if (leftSecondNeighborCellSourceId == -1)
                 {
                     tileMapLayer.SetCell(leftSecondNeighborCell, -1);
                     tileMapLayer.SetCell(leftFirstNeighborCell, -1);
                     tileMapLayer.SetCell(tilePosition, -1);
                     tileMapLayer.SetCell(rightFirstNeighborCell, -1);
                 }
-                if (side == "left")
+            }
+            if (side == "right")
+            {
+                if (rightFirstNeighborCellSourceId == -1)
                 {
-                    if (leftFirstNeighborCellSourceId == -1)
-                    {
-                        tileMapLayer.SetCell(tilePosition, -1);
-                        tileMapLayer.SetCell(rightFirstNeighborCell, -1);
-                    }
-                    if (leftSecondNeighborCellSourceId == -1)
-                    {
-                        tileMapLayer.SetCell(leftSecondNeighborCell, -1);
-                        tileMapLayer.SetCell(leftFirstNeighborCell, -1);
-                        tileMapLayer.SetCell(tilePosition, -1);
-                        tileMapLayer.SetCell(rightFirstNeighborCell, -1);
-                    }
-                }
-                if (side == "right")
-                {
-                    if (rightFirstNeighborCellSourceId == -1)
-                    {
-                        tileMapLayer.SetCell(tilePosition, -1);
-                        tileMapLayer.SetCell(leftFirstNeighborCell, -1);
-                    }
+                    tileMapLayer.SetCell(tilePosition, -1);
+                    tileMapLayer.SetCell(leftFirstNeighborCell, -1);
                 }
             }
+        }
 
-            if (ShellProjectileDirection == Vector2.Left || ShellProjectileDirection == Vector2.Right)
+        if (ShellProjectileDirection == Vector2.Left || ShellProjectileDirection == Vector2.Right)
+        {
+            Vector2I topFirstNeighborCell = tileMapLayer.GetNeighborCell(tilePosition, TileSet.CellNeighbor.TopSide);
+            int topFirstNeighborCellSourceId = tileMapLayer.GetCellSourceId(topFirstNeighborCell);
+
+            Vector2I topSecondNeighborCell = tileMapLayer.GetNeighborCell(topFirstNeighborCell, TileSet.CellNeighbor.TopSide);
+            int topSecondNeighborCellSourceId = tileMapLayer.GetCellSourceId(topSecondNeighborCell);
+
+            Vector2I bottomFirstNeighborCell = tileMapLayer.GetNeighborCell(tilePosition, TileSet.CellNeighbor.BottomSide);
+            int bottomFirstNeighborCellSourceId = tileMapLayer.GetCellSourceId(bottomFirstNeighborCell);
+
+            var side = topFirstNeighborCellSourceId == -1 || topSecondNeighborCellSourceId == -1 ? "left" :
+            bottomFirstNeighborCellSourceId == -1
+            ? "right" :
+            "middle";
+
+            if (side == "middle")
             {
-                Vector2I topFirstNeighborCell = tileMapLayer.GetNeighborCell(tilePosition, TileSet.CellNeighbor.TopSide);
-                int topFirstNeighborCellSourceId = tileMapLayer.GetCellSourceId(topFirstNeighborCell);
-
-                Vector2I topSecondNeighborCell = tileMapLayer.GetNeighborCell(topFirstNeighborCell, TileSet.CellNeighbor.TopSide);
-                int topSecondNeighborCellSourceId = tileMapLayer.GetCellSourceId(topSecondNeighborCell);
-
-                Vector2I bottomFirstNeighborCell = tileMapLayer.GetNeighborCell(tilePosition, TileSet.CellNeighbor.BottomSide);
-                int bottomFirstNeighborCellSourceId = tileMapLayer.GetCellSourceId(bottomFirstNeighborCell);
-
-                var side = topFirstNeighborCellSourceId == -1 || topSecondNeighborCellSourceId == -1 ? "left" :
-                bottomFirstNeighborCellSourceId == -1
-                ? "right" :
-                "middle";
-
-                if (side == "middle")
+                tileMapLayer.SetCell(topSecondNeighborCell, -1);
+                tileMapLayer.SetCell(topFirstNeighborCell, -1);
+                tileMapLayer.SetCell(tilePosition, -1);
+                tileMapLayer.SetCell(bottomFirstNeighborCell, -1);
+            }
+            if (side == "left")
+            {
+                if (topFirstNeighborCellSourceId == -1)
+                {
+                    tileMapLayer.SetCell(tilePosition, -1);
+                    tileMapLayer.SetCell(bottomFirstNeighborCell, -1);
+                }
+                if (topSecondNeighborCellSourceId == -1)
                 {
                     tileMapLayer.SetCell(topSecondNeighborCell, -1);
                     tileMapLayer.SetCell(topFirstNeighborCell, -1);
                     tileMapLayer.SetCell(tilePosition, -1);
                     tileMapLayer.SetCell(bottomFirstNeighborCell, -1);
                 }
-                if (side == "left")
+            }
+            if (side == "right")
+            {
+                if (bottomFirstNeighborCellSourceId == -1)
                 {
-                    if (topFirstNeighborCellSourceId == -1)
-                    {
-                        tileMapLayer.SetCell(tilePosition, -1);
-                        tileMapLayer.SetCell(bottomFirstNeighborCell, -1);
-                    }
-                    if (topSecondNeighborCellSourceId == -1)
-                    {
-                        tileMapLayer.SetCell(topSecondNeighborCell, -1);
-                        tileMapLayer.SetCell(topFirstNeighborCell, -1);
-                        tileMapLayer.SetCell(tilePosition, -1);
-                        tileMapLayer.SetCell(bottomFirstNeighborCell, -1);
-                    }
-                }
-                if (side == "right")
-                {
-                    if (bottomFirstNeighborCellSourceId == -1)
-                    {
-                        tileMapLayer.SetCell(tilePosition, -1);
-                        tileMapLayer.SetCell(topFirstNeighborCell, -1);
-                    }
+                    tileMapLayer.SetCell(tilePosition, -1);
+                    tileMapLayer.SetCell(topFirstNeighborCell, -1);
                 }
             }
-
         }
     }
     #endregion
